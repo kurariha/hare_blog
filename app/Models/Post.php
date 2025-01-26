f7a96c1df4f191b65cf7c697d17585c8b5e00902
kurariha <haiji@gmail.com>
2025-01-26 14:10:20 +0900

    コメント登録機能追加

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -17,6 +17,12 @@ class Post extends Model
         return $this->belongsTo(User::class);
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+
     public function getImageUrlAttribute()
     {
         return Storage::url($this->image_path);
